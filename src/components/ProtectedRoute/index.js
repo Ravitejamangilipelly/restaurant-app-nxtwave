0bc41c46c6a250d824512111b110f30232c519d0
Ravitejamangilipelly <ravitejamangilipelly586@gamail.com>
2024-04-26 07:12:10 +0000

    restaurant-updated

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,14 @@
+import Cookies from 'js-cookie'
+import {Redirect, Route} from 'react-router-dom'
+
+const ProtectedRoute = props => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (!jwtToken) {
+    return <Redirect to="/login" />
+  }
+
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
